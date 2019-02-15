e0e2eb2b91ab75215f83e0cc7b7f940ba08c1875
bovealexandre <alexandre.l.bove@gmail.com>
2019-02-15 14:44:50 +0100

    Envoi du site terminé

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Innovation.io
 
 Venez découvrir notre team dynamique sur [le site](https://www.google.com) .
-# innovation.io
---
+++ script.js
@@ -0,0 +1,39 @@
+
+// import some polyfill to ensure everything works OK
+import "babel-polyfill"
+
+// import bootstrap's javascript part
+import 'bootstrap';
+
+/*
+  Put the JavaScript code you want below.
+*/
+
+console.log("Hey look in your browser console. It works!");
+
+$(document).ready(function(){
+
+  $(".filter-button").click(function(){
+      var value = $(this).attr('data-filter');
+      
+      if(value == "all")
+      {
+          //$('.filter').removeClass('hidden');
+          $('.filter').show('1000');
+      }
+      else
+      {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+          $(".filter").not('.'+value).hide('3000');
+          $('.filter').filter('.'+value).show('3000');
+          
+      }
+  });
+  
+  if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
